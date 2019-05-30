54234a620952e7edda58110c53fc368bcfd1be18
Peter Kelly <pxkelly@hamilton.edu>
2019-05-30 16:54:45 -0400

    ttt.clj is a tester file for the tic-tac-toe program

---
+++ ttt.clj
@@ -0,0 +1,21 @@
+(defn ttt [in]
+  (case
+      (let [rows [[(nth in 0) (nth in 1) (nth in 2)]
+                  [(nth in 3) (nth in 4) (nth in 5)]
+                  [(nth in 6) (nth in 7) (nth in 8)]]
+            cols (apply map vector rows)
+            diags (map #(map % (range 3)) [#((rows %) %) #((rows %) (- 2 %))])
+            lines (concat rows cols diags)]
+      (first (some (comp #{#{"X"} #{"O"}} set) lines)))
+      "X" "X won"
+      "O" "O won"
+      nil "No winner"))
+
+
+(defn tic-tac-toe-input
+  "Makes a Tic Tac Toe input."
+  [in]
+    (case in
+      0 "."
+      1 "O"
+      2 "X"))
