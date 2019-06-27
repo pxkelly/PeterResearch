a16f76642b54e3b23518db2d998dc2e941864a20
Peter Kelly <pxkelly@hamilton.edu>
2019-06-27 19:44:51 -0400

    More testing

--- sudoku.clj
+++ sudoku.clj
@@ -1,3 +1,41 @@
+(defn rotate
+  [n coll]
+  (let [c (count coll)]
+    (take c (drop (mod n c) (cycle coll)))))
+
+(defn create
+  []
+  (let [row (rand-nth (combo/permutations [1 2 3 4 5 6 7 8 9]))]
+    (vector row
+            (vec (rotate -3 row))
+            (vec (rotate -6 row))
+            (vec (rotate -7 row))
+            (vec (rotate -10 row))
+            (vec (rotate -13 row))
+            (vec (rotate -14 row))
+            (vec (rotate -17 row))
+            (vec (rotate -20 row)))))
+
+(defn check
+  [block]
+  (clojure.set/subset? #{1 2 3 4 5 6 7 8 9} (set block)))
+
+(defn square-divide
+  [board]
+  (loop [i 0 squares []]
+    (if-not (< i 9)
+      squares
+      (recur (+ i 3)
+             (into squares
+                   (partition 9 (apply concat (map #(take 3 (drop i %)) board))))))))
+(defn solver
+  [board]
+  (if (and
+        (every? true? (map check board))
+        (every? true? (map check (apply map vector board)))
+        (every? true? (map check (square-divide board))))
+        true false))
+
 (defn solve
   [board]
   (if (not= (count board) 81) false
@@ -15,12 +53,6 @@
                          (partition 9 (vec (apply concat (apply map vector (take 3 (partition 9 board))))))
                          (partition 9 (vec (apply concat (apply map vector (partition 9 (subvec board 27 54))))))
                          (partition 9 (vec (apply concat (apply map vector (nthrest (partition 9 board) 6))))))))))
-
-
-(defn check
-  [block]
-  (clojure.set/subset? #{1 2 3 4 5 6 7 8 9} (set block)))
-
 
   (defn sudoku-test-cases
     "Takes a sequence of inputs and gives IO test cases of the form
--- ttt.clj
+++ ttt.clj
@@ -1,16 +1,20 @@
 (defn ttt [in]
-  (println (nth in 0))
-  (case
-      (let [rows [(nth in 0)
-                  (nth in 1)
-                  (nth in 2)]
-            cols (apply map vector rows)
-            diags (map #(map % (range 3)) [#((rows %) %) #((rows %) (- 2 %))])
-            lines (concat rows cols diags)]
-      (first (some (comp #{#{"X"} #{"O"}} set) lines)))
-      "X" "X won"
-      "O" "O won"
-      "No winner"))
+  (let [rows [(nth in 0)
+              (nth in 1)
+              (nth in 2)]
+        cols (apply map vector rows)
+        diags (map #(map % (range 3)) [#((rows %) %) #((rows %) (- 2 %))])
+        lines (concat rows cols diags)]
+        (cond
+          (apply = (first lines)) [0 0]   ; solution starts at position 0,0
+          (apply = (second lines)) [1 0]  ; starts at 1,0
+          (apply = (nth lines 2)) [2 0]   ; starts at 2,0
+          (apply = (nth lines 3)) [0 0]   ; starts at 0,0
+          (apply = (nth lines 4)) [0 1]   ; starts at 0,1
+          (apply = (nth lines 5)) [0 2]   ; starts at 0,2
+          (apply = (nth lines 6)) [0 1]  ; starts at 0,1
+          (apply = (nth lines 7)) [0 2]  ; starts at 0,2
+          :else [-1 -1]))) ; no answer
 
 ;; Define test cases
 (defn tic-tac-toe-input
